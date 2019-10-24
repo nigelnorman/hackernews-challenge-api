f8b75e9fdab4ae0c6cd3cb917210115f98fd5d77
Nigel Norman <Nigel.Norman@proplogix.com>
2019-10-24 08:55:09 -0400

    adding initial tests

---
+++ HNChallenge.Api/HNChallenge.Api.Tests/Items/ItemsServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using HNChallenge.Api.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HNChallenge.Api.Tests.Items
+{
+    public class ItemsServiceTests
+    {
+        private readonly ItemsService itemsService;
+
+        public ItemsServiceTests()
+        {
+            var usersServiceDependency = new UsersService();
+            this.itemsService = new ItemsService(usersServiceDependency);
+        }
+
+        [Fact]
+        public void GetItemById_success()
+        {
+            const int itemId = 666;
+
+            var item = this.itemsService.GetItemById(itemId);
+
+            Assert.NotNull(item);
+        }
+    }
+}
---
+++ HNChallenge.Api/HNChallenge.Api.Tests/Users/UsersServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using HNChallenge.Api.Entities;
+using HNChallenge.Api.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HNChallenge.Api.Tests.Given_ItemsService_and_no_data
+{
+    public class UsersServiceTests
+    {
+        private readonly UsersService usersService;
+
+        public UsersServiceTests()
+        {
+            this.usersService = new UsersService();
+        }
+
+        [Fact]
+        public void GetUser_success()
+        {
+            // oui, c'est moi
+            const string userId = "nsquared";
+
+            var user = this.usersService.GetUserById(userId);
+
+            Assert.NotNull(user);
+        }
+
+    }
+}
