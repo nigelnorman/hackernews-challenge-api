e270d33dee4a849db13b463f2043607ac5640f03
Nigel Norman <iam.nigelnorman@gmail.com>
2020-02-08 04:02:39 -0500

    resolving package reference versions; resolving mvc options with upgrade to dotnet 3

--- HNChallenge.Api/HNChallenge.Api/Startup.cs
+++ HNChallenge.Api/HNChallenge.Api/Startup.cs
@@ -26,7 +26,12 @@ namespace HNChallenge.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+                .AddMvcOptions(opts =>
+                {
+                    opts.EnableEndpointRouting = false;
+                });
             services.AddCors(opts =>
             {
                 opts.AddPolicy("AllowOrigin",
@@ -40,17 +45,17 @@ namespace HNChallenge.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            //if (env.WebRootPath)
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
 
             app.UseHttpsRedirection();
             app.UseMvc();
